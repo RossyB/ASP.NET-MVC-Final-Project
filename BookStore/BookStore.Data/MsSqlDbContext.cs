f1de2c7f7920a074ad7dee3adb0f60a3c80d5065
Росица Бъндева <rossy@compass-bg.net>
2017-10-03 01:28:33 +0300

    Scaffolding project and extract data layer

---
+++ BookStore/BookStore.Data/MsSqlDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using BookStore.Data.Model;
+
+namespace BookStore.Data
+{
+    public class MsSqlDbContext : IdentityDbContext<User>
+    {
+        public MsSqlDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static MsSqlDbContext Create()
+        {
+            return new MsSqlDbContext();
+        }
+    }
+}
---
+++ BookStore/BookStore.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BookStore.Web.Startup))]
+namespace BookStore.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
